2595f63d7d60447f2f62c94e0f23ef150d7da5d4
vitorlee <lgabc2001@gmail.com>
2015-01-14 11:47:44 +0800

    fixed

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,10 +24,10 @@
             //多文件压缩 !合并
             files: [{
                   expand: true,
-                  cwd: '../trunk/static/js', 
-                  src: ['*.js', '!*.min.js'], 
+                  cwd: '../trunk/static/script', 
+                  src: ['*.js'], 
                   dest: '../trunk/static/js', 
-                  ext: '.min.js' 
+                  ext: '.js' 
               }]
           }
      },
@@ -55,13 +55,6 @@
       },
       files: ['../trunk/static/js/*.js']
     },
-    //复制文件
-    copy: {
-      main: {
-        src: 'max/*',
-        dest: 'src/',
-      },
-    },
     //监测文件变化
     watch: {
       options: {
@@ -69,8 +62,10 @@
           spawn: false,
         },
        codes: {
-        files: ['../trunk/static/js/*.js','../trunk/static/less/*.less'],
+        files: ['../trunk/static/script/*.js','../trunk/static/less/*.less'],
         tasks: ['uglify', 'less'],
+        // files: ['../trunk/static/less/*.less'],
+        // tasks: ['less'],
        }
     }
   });
@@ -78,14 +73,8 @@
   require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
   require('time-grunt')(grunt);
 
-  //载入concat和uglify插件，分别对于合并和压缩
-  // grunt.loadNpmTasks('grunt-contrib-concat');
-  //grunt.loadNpmTasks('grunt-contrib-uglify');
-  // grunt.loadNpmTasks('grunt-contrib-cssmin');
- 
   //注册任务
   grunt.registerTask('default', ['less:production', 'uglify:build']);
   grunt.registerTask('check', ['jshint:files']);
-  //grunt.registerTask('move', ['copy:main']);
   grunt.registerTask('live', ['watch:codes']);
 }
